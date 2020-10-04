2f2fe8f1b4f183d1587df0c31c54c76f4873b6b1
Nishant <nishantchauhan949@hotmail.com>
2020-10-04 19:20:56 +0530

    Tic-Tac-Toe First

---
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -0,0 +1,9 @@
+package tictactoe;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("X O X");
+        System.out.println("X O O");
+        System.out.println("O X X");
+    }
+}
