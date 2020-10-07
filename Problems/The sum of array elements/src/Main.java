c3fdf6ccbfa143c0f5e61e28d321947bb3ff1221
Nishant <nishantchauhan949@hotmail.com>
2020-10-07 17:03:05 +0530

    Sum of Array Complete

--- Problems/The sum of array elements/src/Main.java
+++ Problems/The sum of array elements/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner s = new Scanner(System.in);
+
+        int len = s.nextInt();
+
+        int[] arr = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            arr[i] = s.nextInt();
+        }
+
+        int sum = 0;
+
+        for (int var : arr) {
+            sum += var;
+        }
+
+        System.out.println(sum);
     }
 }
