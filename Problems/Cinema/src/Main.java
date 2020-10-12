069bed42c872fe06ed342b67be32b39b34b367f4
Nishant <nishantchauhan949@hotmail.com>
2020-10-12 19:02:01 +0530

    Cinema Seats Problem Completed

--- Problems/Cinema/src/Main.java
+++ Problems/Cinema/src/Main.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+        int m = s.nextInt();
+
+        int[][] inputArr = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                inputArr[i][j] = s.nextInt();
+            }
+        }
+
+        int numberOfConsecutiveSeatsUserWant = s.nextInt();
+
+//        int rowWhereUserCanGetConsecutiveSeats = 0;
+
+        for (int a = 0; a < n; a++) {
+            int count = 0;
+
+            for (int b = 0; b < m; b++) {
+                if (inputArr[a][b] == 0) {
+                    count++;
+                    if (count == numberOfConsecutiveSeatsUserWant) {
+                        System.out.println(a + 1);
+                        return;
+                    }
+                } else {
+                    count = 0;
+                }
+            }
+
+        }
+
+        System.out.println(0);
     }
 }
